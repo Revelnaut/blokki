c59e16a3ac48f93750dcde1bc8e6c6de46adbfd4
Aapeli Kutila <aapeli.kutila@gmail.com>
2023-11-10 17:40:01 +0200

    Add play button to intro screen

--- intro.gd
+++ intro.gd
@@ -3,6 +3,5 @@ extends Node
 func _ready():
 	%AnimationPlayer.play("icon_bounce")
 
-func _input(event):
-	if event is InputEventMouseButton:
-		get_tree().change_scene_to_file("res://main.tscn")
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
